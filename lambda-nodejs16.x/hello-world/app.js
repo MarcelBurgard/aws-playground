da158326dd3384ef6ed961439329ac8a829b576c
Niels Hanselmann <nha@objektkultur.de>
2023-10-20 20:20:50 +0200

    did some things

--- lambda-nodejs16.x/hello-world/app.js
+++ lambda-nodejs16.x/hello-world/app.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -10,22 +33,76 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.lambdaHandler = void 0;
-let response;
+const AWS = __importStar(require("aws-sdk"));
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const lambdaHandler = (event) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        // const ret = await axios(url);
-        response = {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: "hello world from tsx",
-                // location: ret.data.trim()
-            }),
-        };
+    if (event.httpMethod === 'POST') {
+        // Handle the POST request, i.e., create a new entry
+        return addEmployee(event);
     }
-    catch (err) {
-        console.log(err);
-        throw err;
+    else if (event.httpMethod === 'GET') {
+        // Handle the GET request, i.e., retrieve entries
+        return getEmployees(event);
     }
-    return response;
+    // Send a default response for any other requests
+    return {
+        statusCode: 405,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Method Not Allowed',
+    };
 });
 exports.lambdaHandler = lambdaHandler;
+const addEmployee = (event) => __awaiter(void 0, void 0, void 0, function* () {
+    // Your logic for adding an employee
+    // You will parse the 'event.body' to get the employee details
+    var _a;
+    return {
+        statusCode: 500,
+        body: (_a = event === null || event === void 0 ? void 0 : event.body) !== null && _a !== void 0 ? _a : '',
+    };
+    // // Example of adding an employee:
+    // const requestBody = JSON.parse(event?.body ?? '');
+    // const params = {
+    //   TableName: 'Employees', // the name of your DynamoDB table
+    //   Item: {
+    //     employeeId: v4(), // Generating a new UUID for each employee
+    //     // ... other employee details like name, role, etc.
+    //     employeeName: requestBody.employeeName,
+    //     // other attributes...
+    //   },
+    // };
+    // try {
+    //   await dynamoDb.put(params).promise();
+    //   return {
+    //     statusCode: 200,
+    //     body: JSON.stringify({ message: 'Employee added successfully' }),
+    //   };
+    // } catch (error) {
+    //   // handle potential errors
+    //   return {
+    //     statusCode: 500,
+    //     body: JSON.stringify({ message: 'Internal Server Error' }),
+    //   };
+    // }
+});
+const getEmployees = (event) => __awaiter(void 0, void 0, void 0, function* () {
+    // Your logic for retrieving employees
+    // Example of retrieving all employees:
+    const params = {
+        TableName: 'Employees',
+    };
+    try {
+        const data = yield dynamoDb.scan(params).promise();
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ employees: data.Items }),
+        };
+    }
+    catch (error) {
+        // handle potential errors
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Internal Server Error' }),
+        };
+    }
+});
--- lambda-nodejs16.x/hello-world/app.ts
+++ lambda-nodejs16.x/hello-world/app.ts
@@ -1,22 +1,81 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda/trigger/api-gateway-proxy";
-import { Handler } from "aws-lambda";
+import { APIGatewayProxyEvent, APIGatewayProxyHandler } from 'aws-lambda';
+import { v4 } from 'uuid'; // Importing the UUID v4 function
+import * as AWS from 'aws-sdk';
 
-let response;
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-export const lambdaHandler: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  try {
-    // const ret = await axios(url);
-    response = {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "hello world from tsx",
-        // location: ret.data.trim()
-      }),
-    };
-  } catch (err) {
-    console.log(err);
-    throw err;
+export const lambdaHandler: APIGatewayProxyHandler = async (event) => {
+  if (event.httpMethod === 'POST') {
+    // Handle the POST request, i.e., create a new entry
+    return addEmployee(event);
+  } else if (event.httpMethod === 'GET') {
+    // Handle the GET request, i.e., retrieve entries
+    return getEmployees(event);
   }
 
-  return response;
+  // Send a default response for any other requests
+  return {
+    statusCode: 405,
+    headers: { 'Content-Type': 'text/plain' },
+    body: 'Method Not Allowed',
+  };
+};
+
+const addEmployee = async (event: APIGatewayProxyEvent) => {
+  // Your logic for adding an employee
+  // You will parse the 'event.body' to get the employee details
+
+  return {
+    statusCode: 500,
+    body: event?.body ?? '',
+  }
+
+  // // Example of adding an employee:
+  // const requestBody = JSON.parse(event?.body ?? '');
+  // const params = {
+  //   TableName: 'Employees', // the name of your DynamoDB table
+  //   Item: {
+  //     employeeId: v4(), // Generating a new UUID for each employee
+  //     // ... other employee details like name, role, etc.
+  //     employeeName: requestBody.employeeName,
+  //     // other attributes...
+  //   },
+  // };
+
+  // try {
+  //   await dynamoDb.put(params).promise();
+  //   return {
+  //     statusCode: 200,
+  //     body: JSON.stringify({ message: 'Employee added successfully' }),
+  //   };
+  // } catch (error) {
+  //   // handle potential errors
+  //   return {
+  //     statusCode: 500,
+  //     body: JSON.stringify({ message: 'Internal Server Error' }),
+  //   };
+  // }
+};
+
+const getEmployees = async (event: APIGatewayProxyEvent) => {
+  // Your logic for retrieving employees
+
+  // Example of retrieving all employees:
+  const params = {
+    TableName: 'Employees',
+  };
+
+  try {
+    const data = await dynamoDb.scan(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ employees: data.Items }),
+    };
+  } catch (error) {
+    // handle potential errors
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Internal Server Error' }),
+    };
+  }
 };
