0f874982261819db7d6bf4d9b3183ba53ec1f637
Marcel Burgard <mbu@objektkultur.de>
2023-10-20 19:08:14 +0200

    switch to tsc

--- lambda-nodejs16.x/hello-world/app.js
+++ lambda-nodejs16.x/hello-world/app.js
@@ -10,22 +10,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.lambdaHandler = void 0;
-// const axios = require('axios')
-// const url = 'http://checkip.amazonaws.com/';
 let response;
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- *
- */
-const lambdaHandler = (event, context) => __awaiter(void 0, void 0, void 0, function* () {
+const lambdaHandler = (event) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         // const ret = await axios(url);
         response = {
@@ -38,7 +24,7 @@ const lambdaHandler = (event, context) => __awaiter(void 0, void 0, void 0, func
     }
     catch (err) {
         console.log(err);
-        return err;
+        throw err;
     }
     return response;
 });
--- lambda-nodejs16.x/hello-world/app.ts
+++ lambda-nodejs16.x/hello-world/app.ts
@@ -1,21 +1,9 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda/trigger/api-gateway-proxy";
 import { Handler } from "aws-lambda";
-// const axios = require('axios')
-// const url = 'http://checkip.amazonaws.com/';
+
 let response;
 
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- *
- */
-export const lambdaHandler: Handler = async (event, context) => {
+export const lambdaHandler: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
     // const ret = await axios(url);
     response = {
@@ -27,7 +15,7 @@ export const lambdaHandler: Handler = async (event, context) => {
     };
   } catch (err) {
     console.log(err);
-    return err;
+    throw err;
   }
 
   return response;
